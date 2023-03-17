80a5b01159f2ce64afb8099b6eff6ecb33163cf3
Rafail <myhametshin.rafael@outlook.com>
2023-03-17 21:02:47 +0400

    in

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
-
 async function bootstrap() {
 
   const PORT = process.env.PORT;
