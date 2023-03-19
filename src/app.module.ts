1c04aa9fb7fa5522389207119f2e88c7d6be271b
Rafail <myhametshin.rafael@outlook.com>
2023-03-19 12:27:22 +0400

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
+import { SequelizeModule } from "@nestjs/sequelize";
 import * as path from 'path';
 
 @Module({
@@ -12,6 +13,16 @@ import * as path from 'path';
     }),
     ServeStaticModule.forRoot({
       rootPath: path.resolve(__dirname, 'static')
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MY_SQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      models: [],
+      autoLoadModels: true,
     }),
   ],
   controllers: [AppController],
